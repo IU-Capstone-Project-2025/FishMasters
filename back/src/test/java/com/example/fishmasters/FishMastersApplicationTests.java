bbdf637788f6c51f634b3f43985b5bad370d4f33
plaffyyy <k.zimin@innopolis.university>
2025-06-11 18:45:27 +0300

    feat: add init test

--- back/src/test/java/com/example/fishmasters/FishMastersApplicationTests.java
+++ back/src/test/java/com/example/fishmasters/FishMastersApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class FishMastersApplicationTests {
 
+    //test for checking spring boot correction
     @Test
-    void contextLoads() {
-    }
+    void contextLoads() {}
 
 }
