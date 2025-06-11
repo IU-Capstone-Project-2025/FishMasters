a475396fcec4625777b90105e66895c48a1c9e16
Adel Gaznanov <a.gaznanov@innopolis.university>
2025-06-11 18:07:59 +0300

    init: create init "Hello, World!" endpoint

---
+++ back/src/main/java/com/example/fishmasters/FishMastersApplication.java
@@ -0,0 +1,13 @@
+package com.example.fishmasters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FishMastersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FishMastersApplication.class, args);
+    }
+
+}
