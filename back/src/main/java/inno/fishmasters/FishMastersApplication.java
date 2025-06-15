f8978a082e55874a5baf1f090a8f22d83330d305
plaffyyy <k.zimin@innopolis.university>
2025-06-15 22:22:33 +0300

    feat: add security dependencies and start to realize

--- back/src/main/java/inno/fishmasters/FishMastersApplication.java
+++ back/src/main/java/inno/fishmasters/FishMastersApplication.java
@@ -1,4 +1,4 @@
-package com.example.fishmasters;
+package inno.fishmasters;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- back/src/main/java/inno/fishmasters/controller/SampleController.java
+++ back/src/main/java/inno/fishmasters/controller/SampleController.java
@@ -1,4 +1,4 @@
-package com.example.fishmasters;
+package inno.fishmasters.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
