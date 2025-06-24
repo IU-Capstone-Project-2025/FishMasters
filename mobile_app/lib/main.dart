f86fb7f96b0b9cc5459c4537c0696d19098ccd86
Kirill Greshnov <k.greshnov@innopolis.university>
2025-06-24 13:06:10 +0300

    fix: local storage bug

--- mobile_app/lib/main.dart
+++ mobile_app/lib/main.dart
@@ -8,7 +8,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   final appDocDir = await getApplicationDocumentsDirectory();
   Hive.init(appDocDir.path);
-  Hive.openBox('settings');
+  await Hive.openBox('settings');
   runApp(MainApp());
 }
 
