e8109dc63a3c59857310eab7e116d9254f972ca7
Kirill Greshnov <k.greshnov@innopolis.university>
2025-06-25 08:16:33 +0300

    feat: clear local storage on log out

--- mobile_app/lib/functions/on_start.dart
+++ mobile_app/lib/functions/on_start.dart
@@ -1,4 +1,5 @@
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 Future<bool> hasLoggedInBefore() async {
   final prefs = await SharedPreferences.getInstance();
@@ -7,5 +8,12 @@ Future<bool> hasLoggedInBefore() async {
 
 Future<void> setLoggedIn(bool isLoggedIn) async {
   final prefs = await SharedPreferences.getInstance();
+  if (!isLoggedIn) {
+    if (!Hive.isBoxOpen('settings')) {
+      await Hive.openBox('settings');
+    }
+    final settingsBox = Hive.box('settings');
+    settingsBox.clear();
+  }
   await prefs.setBool('hasLoggedIn', isLoggedIn);
 }
