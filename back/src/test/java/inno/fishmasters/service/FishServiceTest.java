b83011379b70e991b3946388790259b395dfda33
plaffyyy <k.zimin@innopolis.university>
2025-07-01 23:00:26 +0300

    feat: first test with correct pom

---
+++ back/src/test/java/inno/fishmasters/service/FishServiceTest.java
@@ -0,0 +1,14 @@
+package inno.fishmasters.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FishServiceTest {
+
+    @Test
+    void test() {
+        int a = 1;
+        assertEquals(a, 1);
+    }
+}
