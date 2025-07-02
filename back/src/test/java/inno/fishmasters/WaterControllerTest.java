6c84e66e99cedb9ebd41a5a8c31c5959838032b6
Adel Gaznanov <a.gaznanov@innopolis.university>
2025-07-02 18:22:36 +0300

    refactor: replace WaterController on ObjectMapper instead

--- back/src/test/java/inno/fishmasters/WaterControllerTest.java
+++ back/src/test/java/inno/fishmasters/WaterControllerTest.java
@@ -1,5 +1,6 @@
 package inno.fishmasters;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import inno.fishmasters.controller.WaterController;
 import inno.fishmasters.dto.request.water.WaterCreationRequest;
 import inno.fishmasters.entity.Water;
@@ -7,38 +8,46 @@ import inno.fishmasters.service.WaterService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(WaterController.class)
 public class WaterControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
 
     @MockitoBean
     private WaterService waterService;
 
     @Autowired
-    private WaterController waterController;
+    private ObjectMapper objectMapper;
 
     @Test
-    void shouldCreateNewWater() {
+    void shouldCreateNewWater() throws Exception {
         WaterCreationRequest request = new WaterCreationRequest(
                 0.5,
                 0.5
         );
-
         Water water = new Water(
                 1L,
                 0.5,
                 0.5
         );
-
         when(waterService.createWater(request)).thenReturn(water);
 
-        ResponseEntity<Water> response = waterController.createWater(request);
-        assertEquals(200, response.getStatusCode().value());
-        assertEquals(water, response.getBody());
+        mockMvc.perform(post("/api/water/create")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1L))
+                .andExpect(jsonPath("$.x").value(0.5))
+                .andExpect(jsonPath("$.y").value(0.5));
     }
 }
