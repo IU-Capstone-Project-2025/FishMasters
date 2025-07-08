a74e204e3a20db890a7c92a0570fa0386199746e
plaffyyy <k.zimin@innopolis.university>
2025-07-08 22:51:01 +0300

    feat: create 2 endpoints

--- back/src/main/java/inno/fishmasters/controller/WaterController.java
+++ back/src/main/java/inno/fishmasters/controller/WaterController.java
@@ -6,10 +6,9 @@ import inno.fishmasters.service.WaterService;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/water")
@@ -20,11 +19,25 @@ public class WaterController {
         this.waterService = waterService;
     }
 
-    @Operation(summary = "Create a new water point")
+    @Operation(summary = "Create a new water point", description = "ID of water is computed by formula: x * 1000 + y")
     @PostMapping("/create")
     public ResponseEntity<Water> createWater(@Validated @RequestBody WaterCreationRequest request) {
         Water water = waterService.createWater(request);
         return ResponseEntity.ok(water);
+    }
+
+    @Operation(summary = "Get water point by coordinates")
+    @GetMapping("/{id}")
+    public ResponseEntity<Water> getWaterById(@PathVariable Long id) {
+        Water water = waterService.getWaterById(id);
+        return ResponseEntity.ok(water);
+    }
+
+    @Operation(summary = "Get all water points")
+    @GetMapping("/all")
+    public ResponseEntity<List<Water>> getAllWaters() {
+        List<Water> waters = waterService.getAllWaters();
+        return ResponseEntity.ok(waters);
     }
 }
 
