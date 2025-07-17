20c45321bb09de484595f8b73cbf752db53ee516
Adel Gaznanov <a.gaznanov@innopolis.university>
2025-07-17 18:58:35 +0300

    fix: fix user photo type

--- back/src/main/java/inno/fishmasters/dto/request/auth/CreateFisherRequest.java
+++ back/src/main/java/inno/fishmasters/dto/request/auth/CreateFisherRequest.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import org.springframework.web.multipart.MultipartFile;
 
 @Schema(description = "Форма для создания пользователя")
 public record CreateFisherRequest(
@@ -20,6 +21,6 @@ public record CreateFisherRequest(
         @NotNull
         @Size(min = 6, max = 255, message = "Пароль должен быть от 6 до 255 символов")
         String password,
-        String photo
+        MultipartFile photo
 ) {
 }
